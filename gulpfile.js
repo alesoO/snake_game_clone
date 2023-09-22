77d3c8e6a11000c87285e25b8c6554cb7d6471cc
Alexandre Soares <xxalesoaresxx@gmail.com>
2023-09-22 14:20:38 -0300

    new file:   build/js/main.js 	new file:   build/js/maps/main.js.map 	new file:   build/styles/main.css 	new file:   build/styles/maps/main.css.map 	modified:   gulpfile.js 	modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   src/js/main.js 	modified:   src/styles/main.scss 	new file:   src/styles/reset.scss 	modified:   src/styles/var.scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,30 @@
 const gulp = require('gulp');
+const gulpUglify = require('gulp-uglify');
 const sass = require('gulp-sass')(require('sass'));
+const sourceMaps = require('gulp-sourcemaps');
 
-function compSass(){
+function compSass() {
    return gulp.src('./src/styles/main.scss')
-   .pipe(sass())
-   .pipe(gulp.dest('./build/styles')) 
+      .pipe(sourceMaps.init())
+      .pipe(sass({
+         outputStyle: 'compressed'
+      }
+      ))
+      .pipe(sourceMaps.write('./maps'))
+      .pipe(gulp.dest('./build/styles'))
+}
+function compJs() {
+   return gulp.src('./src/js/main.js')
+      .pipe(sourceMaps.init())
+      .pipe(gulpUglify())
+      .pipe(sourceMaps.write('./maps'))
+      .pipe(gulp.dest('./build/js'))
 }
 
 exports.default = compSass;
 exports.compSass = compSass;
+
+exports.watch = function () {
+   gulp.watch('./src/styles/*.scss', gulp.series(compSass));
+   gulp.watch('./src/js/*.js', gulp.series(compJs));
+}
